9a5c845dcd887522331bd90631c31537de5f6c15
matt skeins <mattskeins@matts-MacBook-Pro.local>
2019-06-28 13:42:54 -0400

    zip file for chat bot

---
+++ interfaceMock/Pods/Target Support Files/Pods-interfaceMock/Pods-interfaceMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_interfaceMock : NSObject
+@end
+@implementation PodsDummy_Pods_interfaceMock
+@end
---
+++ interfaceMock/Pods/Target Support Files/Pods-interfaceMockUITests/Pods-interfaceMockUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_interfaceMockUITests : NSObject
+@end
+@implementation PodsDummy_Pods_interfaceMockUITests
+@end
