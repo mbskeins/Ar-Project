b263cc99aa838537599f65437a162f0dac613c9a
matt skeins <mattskeins@matts-MacBook-Pro.local>
2019-07-19 14:21:50 -0400

    scaled animation, added ar text node, added SVProgressHud

--- ArMonster/ArMonster/ViewController.swift
+++ ArMonster/ArMonster/ViewController.swift
@@ -11,6 +11,7 @@ import SceneKit
 import ARKit
 import PromiseKit
 import AVFoundation
+import SVProgressHUD
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
@@ -27,39 +28,164 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var messageText: UITextField!
     
+    @IBOutlet weak var sendButton: UIButton!
+    var modelPlaced = false
+    var center : CGPoint!
+    let arrow = SCNScene(named: "art.scnassets/arrow.scn")!.rootNode
+    let idleScene = SCNScene(named: "art.scnassets/fightFixed.dae")!
+    var positions = [SCNVector3]()
+    
     @IBAction func sendPressed(_ sender: Any) {
+       SVProgressHUD.show()
+        idleScene.rootNode.enumerateChildNodes { (node, stop) in
+            if node.name == "remove" {
+                node.removeFromParentNode()
+            }
+        }
         newRequest(messageText.text!)
+ 
     }
 
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var animations = [String: CAAnimation]()
+    var idle:Bool = true
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        center = view.center
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let hitTest = sceneView.hitTest(center, types: .featurePoint)
         
-        // Set the view's delegate
-        sceneView.delegate = self
+        let result = hitTest.last
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        guard let transform = result?.worldTransform else {return}
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/idle.scn")!
+        let thirdColumn = transform.columns.3
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let position = SCNVector3Make(thirdColumn.x, thirdColumn.y, thirdColumn.z)
         
-        setInitalState()
+        positions.append(position)
         
-   
+        let lastTenPositions = positions.suffix(10)
+        
+        arrow.position = getAveragePosition(from: lastTenPositions)
 
     }
     
-    func speakText(say messageResponse : String){
+    func getAveragePosition(from positions : ArraySlice<SCNVector3>) -> SCNVector3 {
         
-        let voice = AVSpeechSynthesisVoice(identifier: AVSpeechSynthesisVoiceIdentifierAlex)
-        let myUtterance = AVSpeechUtterance(string: messageResponse)
-        let synth = AVSpeechSynthesizer()
-        myUtterance.voice = voice
-        synth.speak(myUtterance)
+        var averageX : Float = 0
+        var averageY : Float = 0
+        var averageZ : Float = 0
+        
+        for position in positions {
+            
+            averageX += position.x
+            averageY += position.y
+            averageZ += position.z
+        }
+        let count = Float(positions.count)
+        return SCNVector3Make(averageX / count, averageY / count, averageZ / count)
+    }
+    
+    override func viewDidLoad() {
+        idleScene.rootNode.scale = SCNVector3(0.004, 0.004, 0.004)
+        sendButton.layer.cornerRadius = 7
+
+        super.viewDidLoad()
+        sceneView.delegate = self
+        center = view.center
+        
+        // Create a new scene
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        sceneView.scene.rootNode.addChildNode(arrow)
+        
+        // Load the DAE animations
+        //loadAnimations()
+    }
+    
+
+
+    
+    func loadAnimations () {
+        // Load the character in the idle animation
+        
+        // This node will be parent of all the animation models
+        let node = SCNNode()
+        
+        // Add all the child nodes to the parent node
+        for child in idleScene.rootNode.childNodes {
+            node.addChildNode(child)
+        }
+        
+        // Set up some properties
+        node.position = SCNVector3(0, -10, -20)
+        node.scale = SCNVector3(0.08, 0.08, 0.08)
+        
+        // Add the node to the scene
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        // Load all the DAE animations
+        loadAnimation(withKey: "dancing", sceneName: "art.scnassets/fightFixed", animationIdentifier: "fightAnim")
+    }
+    
+    func loadAnimation(withKey: String, sceneName:String, animationIdentifier:String) {
+        let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae")
+        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
+        
+        if let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self) {
+            // The animation will only play once
+            animationObject.repeatCount = 1
+            // To create smooth transitions between animations
+            animationObject.fadeInDuration = CGFloat(1)
+            animationObject.fadeOutDuration = CGFloat(0.5)
+            
+            // Store the animation for later use
+            animations[withKey] = animationObject
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        if modelPlaced == false {
+            guard let angle = sceneView.session.currentFrame?.camera.eulerAngles.y else { return }
+            idleScene.rootNode.position = arrow.position
+            idleScene.rootNode.eulerAngles.y = angle
+            modelPlaced = true
+            sceneView.scene.rootNode.addChildNode(idleScene.rootNode)
+            arrow.removeFromParentNode()
+        }
+//        let location = touches.first!.location(in: sceneView)
+//
+//        // Let's test if a 3D Object was touch
+//        var hitTestOptions = [SCNHitTestOption: Any]()
+//        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+//
+//        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+//
+//        if hitResults.first != nil {
+//            if(idle) {
+//                playAnimation(key: "dancing")
+//            } else {
+//                stopAnimation(key: "dancing")
+//            }
+//            idle = !idle
+//            return
+//        }
+    }
+    func playAnimation(key: String) {
+        // Add the animation to start playing it right away
+        sceneView.scene.rootNode.addAnimation(animations[key]!, forKey: key)
+    }
+    
+    func stopAnimation(key: String) {
+        // Stop the animation with a smooth transition
+        idleScene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -67,9 +193,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+    }
+    
+    
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardSize.height
+            print(keyboardHeight)
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -77,6 +220,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    
+    func speakText(say messageResponse : String){
+        let textGeometry = SCNText(string: messageResponse, extrusionDepth: 1)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.blue
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.scale = SCNVector3(0.75, 1, 0.75)
+        var modelPos = idleScene.rootNode.position
+        modelPos.y += 175
+        modelPos.x -= 75
+        textNode.position = modelPos
+        textNode.name = "remove"
+        idleScene.rootNode.addChildNode(textNode)
+        
+        let voice = AVSpeechSynthesisVoice(identifier: AVSpeechSynthesisVoiceIdentifierAlex)
+        let myUtterance = AVSpeechUtterance(string: messageResponse)
+        print(myUtterance)
+        let synth = AVSpeechSynthesizer()
+        myUtterance.voice = voice
+        //  need to make assignment from distance to 3d object
+        myUtterance.volume = 100
+        synth.speak(myUtterance)
     }
 
     func newRequest(_ text: String) {
@@ -95,6 +261,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             }.catch { (error) in
                 //catch error
         }
+       
         
     }
     
@@ -114,6 +281,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             }
             
         }
+         SVProgressHUD.dismiss()
     }
     // MARK : Detect textfield changes
     @objc func textFieldDidChange(_ textField: UITextField) {
